ac6b0fb7d4d421542df373b12e6138627cc519ed
1Makaka <154027185+1Makaka@users.noreply.github.com>
2026-09-18 17:55:04 +0200

    Change server port to 3000 and clean up comments
    Updated server port from 8080 to 3000 and removed commented lines.

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const players = {};
 io.on('connection', (socket) => {
     console.log('Игрок подключился:', socket.id);
 
-    // Создаем профиль нового игрока
     players[socket.id] = {
         id: socket.id,
         x: 0, y: 0, z: 0,
@@ -23,33 +22,26 @@ io.on('connection', (socket) => {
         nickname: 'Игрок'
     };
 
-    // Отправляем новому игроку инфу обо всех, кто уже на сервере
     socket.emit('currentPlayers', players);
-    // Говорим всем остальным, что зашел новый игрок
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    // Принимаем никнейм
     socket.on('setNickname', (name) => {
         players[socket.id].nickname = name;
         io.emit('updateNickname', { id: socket.id, nickname: name });
     });
 
-    // Синхронизация движения
     socket.on('playerMove', (data) => {
         players[socket.id].x = data.x;
         players[socket.id].z = data.z;
         players[socket.id].rotation = data.rotation;
-        // Отправляем всем, кроме самого отправителя
         socket.broadcast.emit('playerMoved', players[socket.id]);
     });
 
-    // Синхронизация анимаций (дрочка)
     socket.on('playerAnim', (anim) => {
         players[socket.id].anim = anim;
         socket.broadcast.emit('playerAnimChanged', { id: socket.id, anim: anim });
     });
 
-    // Синхронизация стрельбы
     socket.on('shoot', (data) => {
         socket.broadcast.emit('otherPlayerShoot', { 
             id: socket.id, 
@@ -58,7 +50,6 @@ io.on('connection', (socket) => {
         });
     });
 
-    // Отключение
     socket.on('disconnect', () => {
         console.log('Игрок отключился:', socket.id);
         delete players[socket.id];
@@ -66,8 +57,8 @@ io.on('connection', (socket) => {
     });
 });
 
-// Fly.io использует порт 8080 по умолчанию
-const PORT = process.env.PORT || 8080;
+// Используем порт 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер работает на порту ${PORT}`);
 });
